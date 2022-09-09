297ad70b3a5cd292a4cdb450ac5ce5987bf17e3d
Anup Rawal <rwlanup@gmail.com>
2022-09-09 13:47:23 +0545

    remove unnecessary config from postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,4 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   plugins: {
     'tailwindcss/nesting': {},
     tailwindcss: {},
