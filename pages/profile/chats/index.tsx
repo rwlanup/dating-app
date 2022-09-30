15d86a8da2e9c6baa5d4a68d7bb86718d030be0c
Anup Rawal <rwlanup@gmail.com>
2022-09-30 11:16:43 +0545

    fixes ui on mobile

--- pages/profile/chats/index.tsx
+++ pages/profile/chats/index.tsx
@@ -1,9 +1,12 @@
 import { Box, Grid, Theme, useMediaQuery } from '@mui/material';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { ChatFriendsList } from '../../../components/pages/chat-friends-list/ChatFriendsList';
 import { ChatMessage } from '../../../components/pages/chat-message/ChatMessage';
 
 const ChatsPage: NextPage = () => {
+  const { query } = useRouter();
+  const friendId = typeof query.id === 'string' ? query.id : undefined;
   const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
 
   return (
@@ -21,32 +24,35 @@ const ChatsPage: NextPage = () => {
         container
         sx={{ height: 1 }}
       >
-        <Grid
-          item
-          xs={12}
-          lg={4}
-          sx={{ position: 'relative' }}
-        >
-          <Box
-            sx={{
-              position: { lg: 'absolute' },
-              left: 0,
-              top: 0,
-              bottom: 0,
-              width: 1,
-              maxHeight: 1,
-              overflowY: 'scroll',
-              borderRight: { lg: 2 },
-              borderColor: { lg: 'divider' },
-              bgcolor: 'primary.50',
-            }}
-          >
-            <ChatFriendsList />
-          </Box>
-        </Grid>
-        {isDesktop && (
+        {!(!isDesktop && friendId) && (
           <Grid
             item
+            xs={12}
+            lg={4}
+            sx={{ position: 'relative' }}
+          >
+            <Box
+              sx={{
+                position: { lg: 'absolute' },
+                left: 0,
+                top: 0,
+                bottom: 0,
+                width: 1,
+                maxHeight: 1,
+                overflowY: 'scroll',
+                borderRight: { lg: 2 },
+                borderColor: { lg: 'divider' },
+                bgcolor: 'primary.50',
+              }}
+            >
+              <ChatFriendsList />
+            </Box>
+          </Grid>
+        )}
+        {(isDesktop || friendId) && (
+          <Grid
+            item
+            xs={12}
             lg={8}
             sx={{ position: 'relative' }}
           >
